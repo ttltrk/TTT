8d738a212267b641bcda720bb08f2ab205566407
Attila.Torok <attila.torok@ibm.com>
2020-02-07 11:48:58 +0100

    01.01

---
+++ ttt.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+#test 2020-FEB-07
+
+$|=1;
+
+sub main {
+    my $file = 'C:\Users\AttilaTorok\Desktop\ADM\MM\PL\REGEX\mymanjeeves.txt';
+    open(INPUT, $file);
+}
+
+main();
